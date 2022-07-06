32f806303683d959c53e504d3b01adf3beb3c608
xaviguasch <xaviguasch@gmail.com>
2022-07-06 18:23:36 +0200

    toyed with validations and Schema constrains

--- index.js
+++ index.js
@@ -13,16 +13,54 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    maxLength: 20,
   },
-  price: { type: Number },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  onSale: {
+    type: Boolean,
+    default: false,
+  },
+  categories: [String],
+  qty: {
+    online: {
+      type: Number,
+      default: 0,
+    },
+    inStore: {
+      type: Number,
+      default: 0,
+    },
+  },
 })
 
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({ name: 'Mountain Bike', price: 599 })
+// const bike = new Product({
+//   name: 'Soft Helmet',
+//   price: 30.5,
+//   categories: ['Cycling', 'Safety'],
+// })
 
-bike
-  .save()
+// bike
+//   .save()
+//   .then((data) => {
+//     console.log('IT WORKED!')
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log('OH NO ERROR!!!')
+//     console.log(err)
+//   })
+
+Product.findOneAndUpdate(
+  { name: 'Soft Helmet' },
+  { price: -5 },
+  { new: true, runValidators: true }
+)
   .then((data) => {
     console.log('IT WORKED!')
     console.log(data)
